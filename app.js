8e8274c98cc4b1e8e186bbd3a3f901cfd4e446ec
Taras <tmasnyk@gmail.com>
2018-06-21 09:19:53 +0300

    First stage ended. Console program ready

--- app.js
+++ app.js
@@ -33,9 +33,6 @@ console.log('***********************************');
 
 
 stdin.addListener("data", function (d) {
-    // note:  d is an object, and when converted to a string it will
-    // end with a linefeed.  so we (rather crudely) account for that
-    // with toString() and then trim()
     console.log("you entered: [" +
         d.toString().trim() + "]");
     parseLine(d.toString().trim())
